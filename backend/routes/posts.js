4bc280a94db8b1eacf4945a81a98abc7f3f16b8b
Franck Panou <85650372+FPan7792@users.noreply.github.com>
2022-06-08 23:57:17 +0200

    bootstrap the app

---
+++ backend/routes/posts.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/posts", async (req, res) => {
+  res.status(400).send("OK !!!");
+});
+
+// CREATION
+router.post("/posts/create", async (req, res) => {});
+
+// SUPRESSION
+router.post("/posts/delete", async (req, res) => {});
+
+// MOFIFICATIONS
+router.post("/posts/modify", async (req, res) => {});
+
+// LIKES
+router.post("/posts/like", async (req, res) => {});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const port = process.env.PORT || 3003;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// initialisation d'entetes
+app.use(cors());
+
+// conversion des données en JSON
+app.use(express.json());
+
+const authRoutes = require("./routes/auth");
+const postsRoutes = require("./routes/posts");
+
+app.use(authRoutes);
+app.use(postsRoutes);
+
+// si Route inexistante
+app.all("*", (req, res) => {
+  res.status(404).send("Page non trouvée");
 });
 
 app.listen(port, () => {
