49060c79ea6e80a75971617612bbaa6153657d90
Franck Panou <85650372+FPan7792@users.noreply.github.com>
2022-06-18 11:06:54 +0200

    impl: chakra/router wip eslint config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
+// gestion des css components
+import { ChakraProvider } from "@chakra-ui/react";
+import { Flex, Spacer } from "@chakra-ui/react";
+
+// gestion de la navigation et des pages
+import { Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ChakraProvider>
+      <Flex bgColor={"#F5F5F5"} height={"100vh"}>
+        <Routes>
+          <h1>Hello World</h1>
+        </Routes>
+      </Flex>
+    </ChakraProvider>
   );
 }
 
 export default App;
+
+// si le useToken est present , on montre l'application sinon seules
+// les pages de connexion et de signup sont dispos
+
+// set up config eslint && prettier
