cc97777487524436ac6e95b7686fc0f4808650ae
Franck Panou <85650372+FPan7792@users.noreply.github.com>
2022-06-18 12:21:13 +0200

    set eslint + prettier ok

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,20 +6,18 @@ import { Flex, Spacer } from "@chakra-ui/react";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <ChakraProvider>
-      <Flex bgColor={"#F5F5F5"} height={"100vh"}>
-        <Routes>
-          <h1>Hello World</h1>
-        </Routes>
-      </Flex>
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider>
+			<Flex bgColor="#F5F5F5" height="100vh">
+				<Routes>
+					<h1>Hello World</h1>
+				</Routes>
+			</Flex>
+		</ChakraProvider>
+	);
 }
 
 export default App;
 
 // si le useToken est present , on montre l'application sinon seules
 // les pages de connexion et de signup sont dispos
-
-// set up config eslint && prettier
